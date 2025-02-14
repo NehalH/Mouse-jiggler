43cc8391c8fc63ef4e9616076e6cd1890f98f55f
NehalH <nhosalikar@gmail.com>
2025-02-14 20:17:11 +0530

    update: Readme file, script

--- mouseJiggler.ino
+++ mouseJiggler.ino
@@ -36,6 +36,12 @@ void loop() {
   //for compatability with other libraries use DigiMouse.move(X, Y, scroll, buttons)
 }
 
+/**
+ * @brief  Moves pointer in X axis
+ * 
+ * @param pixels Number of pixels to scroll
+ * @param direction 'l' to move pointer left and 'r' to move right
+ */
 void moveX(int pixels, char direction) {
   for(int i = pixels; i > 0; i--){
     DigiMouse.delay(10);
@@ -46,6 +52,12 @@ void moveX(int pixels, char direction) {
   }
 }
 
+/**
+ * @brief Moves pointer in Y axis
+ * 
+ * @param pixels Number of pixels to scroll
+ * @param direction  'u' to move pointer up and 'd' to move down
+ */
 void moveY(int pixels, char direction) {
   for(int i = pixels; i > 0; i--){
     DigiMouse.delay(10);
@@ -56,6 +68,12 @@ void moveY(int pixels, char direction) {
   }
 }
 
+/**
+ * @brief  Scrolls in Y axis
+ * 
+ * @param pixels Number of pixels to scroll
+ * @param direction 'u' for scroll up and 'd' for scroll down
+ */
 void scroll(int pixels, char direction) {
   for(int i = pixels; i > 0; i--){
     DigiMouse.delay(10);
